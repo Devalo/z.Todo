02bfa29ebc1b65a86c4507daae2fee3c5df48c4d
Stephan Valois <stephanvalois@gmail.com>
2016-02-07 15:13:33 +0100

    laget meny

--- todo.rb
+++ todo.rb
@@ -1,26 +1,55 @@
 #!/usr/bin/env ruby
 class Todo
   def initialize(item)
-    @item = item
+    @item = []
   end
 
+
+  def add(todo)
+
+  end
   def to_s
 
   end
 end
 
+class Item
+
+end
+
 def intro
-  puts "hei"
+  line =  "-----------------------------------------"
+  puts line
+  puts "Welcome to Todo, by Devalo"
+  puts line
+#  puts "\n"
+  puts "Usage:"
+#  puts line
+  puts " add 'todo'  | Adds a new todo"
+  puts " delete 'n'  | Deletes a todo"
+  puts " edit 'n'    | Edit a todo"
+  puts " done 'todo' | Mark todo as done"
+  puts " list        | List all todos"
+  puts " clear       | Clear the todolist"
+  puts line
+
+
 end
 
 case ARGV[0]
   when 'add', 'a'
-    puts "Legge til nytt item"
+    if ARGV[1] == nil
+      puts "No todo added. To add todo, use: 'add', followed with your todo. "
+    else
+    puts "Lagt til todo: #{ARGV[1]}"
+    end
   when 'list', 'l'
     puts "Vise liste over todo items"
+  when 'done'
+    puts "Ferdig med todo"
   when 'delete', 'del', 'd'
     puts "Slette valgt todo"
-  when 'edit' 'e'
+  when 'edit', 'e'
     puts "Edit valgte todo"
   when 'clear'
     puts "Slette hele todolista"
