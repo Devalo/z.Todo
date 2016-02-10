9d9367c79fd19c61b9a5d7e7f4e821b81385ee79
Stephan Valois <stephanvalois@gmail.com>
2016-02-10 19:41:46 +0100

    test add todo

--- todo_test.rb
+++ todo_test.rb
@@ -4,15 +4,18 @@ require File.join(File.dirname(__FILE__), 'todo.rb')
 
 describe Todo do
   before do
+    number = 0
+    Todo.new.clear
     @todo = Todo.new
     @todo.clear
+    5.times do
+      @todo.add("Dette er todo nr #{number += 1}")
+    end
      end
 
   it "adds a new todo" do
-    @todo.add 'Dette er todo nr 5'
-    @todo.items.size.must_equal 1
-    @todo.list
-    Todo.new.clear
+    @todo.add("En ny todo")
+    @todo.items.size.must_equal 6
   end
 
 #  it 'lists all todoitems' do
